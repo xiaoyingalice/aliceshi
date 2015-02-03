e83cee5b1401443a864ac197e6e9a467050a3c37
Shi Xiaoying <shixiaoying@ShideMBP.lan>
2015-02-03 10:00:57 +0800

    add Day1

---
+++ Day1/playground1.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+1. “存储方式不一样”
+2. 查询存储的信息
+3. 我不知道，也许要改太麻烦了。
+4. var i(int)
+if i % 2 = "0" {
+    do even number
+} else if i % 2 = "1" {
+    do odd number
+}
+5. 百度百科：二维数组本质上是以数组作为数组元素的数组，即“数组的数组”。
+Swift中，一个数组里有两个discontinuous的区间就叫二维数组吧。。。
+6.不会T_T
+*/
+
+
